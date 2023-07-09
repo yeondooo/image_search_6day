550c8d1a3c3f5e4df7033d9be9c653ca769460e0
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-10 00:22:46 +0900

    feat: main.dart router와 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_6day/router/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,10 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
