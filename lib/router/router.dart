5d1da6f2e2fb7510f86750292ebb475207561468
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-10 00:22:14 +0900

    feat: router 기능 구현

---
+++ lib/router/router.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_search_6day/data/model/photo.dart';
+import 'package:image_search_6day/data/repository/pixabay_photo_repository_impl.dart';
+import 'package:image_search_6day/ui/detail/detail_screen.dart';
+import 'package:image_search_6day/ui/main/main_screen.dart';
+import 'package:image_search_6day/ui/main/main_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return ChangeNotifierProvider(
+          create: (_) => MainViewModel(PixabayPhotoRepositoryImpl()),
+          child: const MainScreen(),
+        );
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'detail',
+          builder: (BuildContext context, GoRouterState state) {
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
+          },
+        ),
+      ],
+    ),
+  ],
+);
